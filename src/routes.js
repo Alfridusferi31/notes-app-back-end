c959e0c82807d899d16f10b787ca4e9612b89e22
Alfridus Elman <alfridusferi@gmail.com>
2024-10-25 10:46:39 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,55 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
+
+// const { addNoteHandler } = require('./handler');
+// const routes = [
+//   {
+//     method: 'POST',
+//     path: '/notes',
+//     handler: addNoteHandler,
+//     // eslint-disable-next-line no-dupe-keys
+//     handler: () => {},
+//   },
+// ];
+
+// module.exports = routes;
